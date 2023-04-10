610d52490ff69e2785f8240e5fb8b1a62a5621f9
tenkei-a <k1.abe0423@gmail.com>
2023-04-10 14:34:51 +0900

    rename

---
+++ javascript/escape-sequence.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// escape sequence
+// console.log('Hello I'm K1.');
+// ''を表示する場合は""で囲む
+console.log('Hello I\'m K1.');
+console.log("Hello I'm K1.");
+
+console.log('\¥ \t AAA \n BBB');
+
+// ""を表示させるときは''で囲む
+console.log('"K1"-san');
+
+// ' or " どっちでもいい
+// 特にこだわりがない場合は''のほうが良い
+// googleの人は''のほうを使用しているらしい
+// ""を表示させるときはエスケープ処理を行う
+console.log('Hello');
+console.log("Hello");
+
