bc991acfaab824ad9ad197c7d46063aac3442b0e
tenkei-a <k1.abe0423@gmail.com>
2023-04-10 15:39:34 +0900

    add loop.js

---
+++ javascript/loop.js
@@ -0,0 +1,20 @@
+"use strict";
+// for文
+for (let i = 1; i <= 3; i++) {
+  console.log(i);
+}
+
+// while文
+let j = 1;
+while (j <= 3) {
+  console.log(j);
+  j++;
+}
+
+// do while文
+// whileとの違いは必ず1回はループを実行する
+let k = 1;
+do {
+  console.log(k);
+  k++;
+} while (k <= 3);
