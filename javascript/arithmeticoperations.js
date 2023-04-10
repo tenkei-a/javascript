ef7e4c8433e03527350a239fa1d919f7e825d35f
tenkei-a <k1.abe0423@gmail.com>
2023-04-10 13:58:00 +0900

    change

---
+++ javascript/arithmeticoperations.js
@@ -0,0 +1,10 @@
+//strictモード:より厳密なエラーチェックを行う。設定推奨
+'use strict';
+
+console.log('Hello World!')
+
+console.log(10+3);
+console.log(10-3);
+console.log(10*3);
+console.log(10/3);
+console.log(10%3);
--- main.js
+++
@@ -1,4 +0,0 @@
-/ strictモード:より厳密なエラーチェックを行う。設定推奨 /
-'use strict';
-
-console.log('Hello World! from main.js')
