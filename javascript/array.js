b3f8fe1df98967f3c30a2191b47bb5aa61da042e
tenkei-a <k1.abe0423@gmail.com>
2023-04-10 16:28:40 +0900

    add array.js

---
+++ javascript/array.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const colors = ['Red','Green','Blue'];
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+
+for(let i = 0; i < colors.length; i++){
+    console.log(colors[i]);
+}
