a35eeb29612ba68ba224e4856d3cbd0f1fb21a45
tenkei-a <k1.abe0423@gmail.com>
2023-04-10 14:23:11 +0900

    add namingrule.js

---
+++ javascript/namingrule.js
@@ -0,0 +1,13 @@
+'use strict'
+// 命名規則はローワーキャメルケース
+// 名前を分かりやすいものにする
+
+let color;
+let colorName;
+let myColorName;
+
+// 変数の宣言は大文字小文字は分けて判断される
+let foo = 'small';
+let Foo = 'big';
+console.log(foo);
+console.log(Foo);
