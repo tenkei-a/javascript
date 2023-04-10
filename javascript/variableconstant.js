841d107ba69d1343c984718b305e3e7377e5b295
tenkei-a <k1.abe0423@gmail.com>
2023-04-10 14:16:43 +0900

    coding

--- javascript/variableconstant.js
+++ javascript/variableconstant.js
@@ -2,6 +2,28 @@
 
 // 変数の宣言
 let x;
-console.log(x);
 x=1;
 console.log(x);
+x=2;
+console.log(x);
+
+let y = 10;
+console.log(y);
+
+let str = 'Hello JavaScript';
+console.log(str);
+
+// let i;
+// let j;
+// 変数の複数宣言
+// let i,j;
+
+// 定数の宣言
+// 定数は宣言時に初期化が必要
+const a = 1;
+console.log(a);
+// 再代入をする場合はTypeError
+// a = 2;
+
+const prefecture = 'Tokyo';
+console.log(prefecture);
