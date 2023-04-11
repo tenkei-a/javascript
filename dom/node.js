dced3dd93209f9fe131ffe7b5007a3df11b4a381
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 13:37:46 +0900

    coding setAttribute,replaceChil

--- dom/node.js
+++ dom/node.js
@@ -1,23 +1,49 @@
 "use strict";
 
 // createElement,createTextNode,appendChild
+// const e = document.getElementById("button");
+// e.addEventListener(
+//   "click",
+//   () => {
+//     // nodeの作成
+//     const li = document.createElement("li");
+//     const text = document.createTextNode("test");
+
+//     // appendChild:指定された要素を現在の要素の最後の子要素として追加する
+//     // <li>test</li>完成
+//     li.appendChild(text);
+
+//     // <ul id="list"></ul>を持ってくる
+//     const listElement = document.getElementById("list");
+
+//     // <ul id="list"></ul>に<li>test</li>が追加される
+//     listElement.appendChild(li);
+//   },
+//   false
+// );
+
+
+// setAttribute,replaceChil
 const e = document.getElementById("button");
 e.addEventListener(
   "click",
   () => {
-    // nodeの作成
-    const li = document.createElement("li");
-    const text = document.createTextNode("test");
+    // li要素の生成<li></li>
+    const newList = document.createElement("li");
+    // 生成したli要素にid要素(newList)を付与<li id="newList"></li>
+    newList.setAttribute("id", "newList");
 
-    // appendChild:指定された要素を現在の要素の最後の子要素として追加する
-    // <li>test</li>完成
-    li.appendChild(text);
+    // textノードの生成
+    const newText = document.createTextNode('new element');
+    // 生成したノードを空のli要素に追加<li id="newList">new element</li>
+    newList.appendChild(newText);
 
-    // <ul id="list"></ul>を持ってくる
-    const listElement = document.getElementById("list");
-
-    // <ul id="list"></ul>に<li>test</li>が追加される
-    listElement.appendChild(li);
+    // 置換前のoldListを参照
+    const oldList = document.getElementById('oldList');
+    // oldListの親ノード<ul></ul>を参照
+    const parentNode = oldList.parentNode;
+    // 置換処理(<li id="newList">new element</li> ← <li id="oldList">old element</li>)
+    parentNode.replaceChild(newList,oldList);
   },
   false
 );
