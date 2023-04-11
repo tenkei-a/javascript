925b0c276d7fba168b99793ecc235199079f53d6
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 14:57:20 +0900

    coding onload

--- dom/event-handler.js
+++ dom/event-handler.js
@@ -1,6 +1,14 @@
 "use strict";
-const e = document.getElementById("button");
-// onclick:クリック時の動作
-e.onclick = () => {
-  console.log("click");
+
+// onclick
+// const e = document.getElementById("button");
+// // onclick:クリック時の動作
+// e.onclick = () => {
+//   console.log("click");
+// };
+
+// onload
+// ロード時の処理
+window.onload = () => {
+  console.log("load event");
 };
