83ce46036355bc8daaa2522bfabb9067680f6abc
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 13:08:13 +0900

    coding createElement,createTextNode,appendChild

--- dom/node.js
+++ dom/node.js
@@ -1,1 +1,23 @@
-'use strict'
+"use strict";
+
+// createElement,createTextNode,appendChild
+const e = document.getElementById("button");
+e.addEventListener(
+  "click",
+  () => {
+    // nodeの作成
+    const li = document.createElement("li");
+    const text = document.createTextNode("test");
+
+    // appendChild:指定された要素を現在の要素の最後の子要素として追加する
+    // <li>test</li>完成
+    li.appendChild(text);
+
+    // <ul id="list"></ul>を持ってくる
+    const listElement = document.getElementById("list");
+
+    // <ul id="list"></ul>に<li>test</li>が追加される
+    listElement.appendChild(li);
+  },
+  false
+);
