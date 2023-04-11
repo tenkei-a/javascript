ac186d494a44a86c228eea41048376f2dc50efb9
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 11:51:17 +0900

    coding getElementsByName

--- dom/getElement.js
+++ dom/getElement.js
@@ -14,17 +14,30 @@
 // );
 
 // getElementsByTagName
+// const e = document.getElementById("button");
+// e.addEventListener(
+//     // クリック時に処理を行う
+//   "click",
+//   () => {
+//     // クリック時の処理
+//     // elementsには複数の要素が入ってくる。配列と似た要素が入ってくる。
+//     const elements = document.getElementsByTagName("div");
+//     for (let i = 0; i < elements.length; i++) {
+//       console.log(elements[i].innerText);
+//     }
+//   },
+//   false
+// );
+
+
+//getElementsByName
 const e = document.getElementById("button");
 e.addEventListener(
-    // クリック時に処理を行う
   "click",
   () => {
-    // クリック時の処理
-    // elementsには複数の要素が入ってくる。配列と似た要素が入ってくる。
-    const elements = document.getElementsByTagName("div");
-    for (let i = 0; i < elements.length; i++) {
-      console.log(elements[i].innerText);
-    }
+    const elements = document.getElementsByName("result");
+    // テキストに入力された内容を受け取るため、valueが必要
+    console.log(elements[0].value);
   },
   false
 );
