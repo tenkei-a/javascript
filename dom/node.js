7fb796afc774eb73cf8549b2fed1a58a8bdf55a8
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 13:51:32 +0900

    coding removeChild

--- dom/node.js
+++ dom/node.js
@@ -22,28 +22,44 @@
 //   false
 // );
 
-
 // setAttribute,replaceChil
+// const e = document.getElementById("button");
+// e.addEventListener(
+//   "click",
+//   () => {
+//     // li要素の生成<li></li>
+//     const newList = document.createElement("li");
+//     // 生成したli要素にid要素(newList)を付与<li id="newList"></li>
+//     newList.setAttribute("id", "newList");
+
+//     // textノードの生成
+//     const newText = document.createTextNode('new element');
+//     // 生成したノードを空のli要素に追加<li id="newList">new element</li>
+//     newList.appendChild(newText);
+
+//     // 置換前のoldListを参照
+//     const oldList = document.getElementById('oldList');
+//     // oldListの親ノード<ul></ul>を参照
+//     const parentNode = oldList.parentNode;
+//     // 置換処理(<li id="newList">new element</li> ← <li id="oldList">old element</li>)
+//     parentNode.replaceChild(newList,oldList);
+//   },
+//   false
+// );
+
+// removeChild
 const e = document.getElementById("button");
 e.addEventListener(
   "click",
   () => {
-    // li要素の生成<li></li>
-    const newList = document.createElement("li");
-    // 生成したli要素にid要素(newList)を付与<li id="newList"></li>
-    newList.setAttribute("id", "newList");
-
-    // textノードの生成
-    const newText = document.createTextNode('new element');
-    // 生成したノードを空のli要素に追加<li id="newList">new element</li>
-    newList.appendChild(newText);
-
-    // 置換前のoldListを参照
-    const oldList = document.getElementById('oldList');
-    // oldListの親ノード<ul></ul>を参照
-    const parentNode = oldList.parentNode;
-    // 置換処理(<li id="newList">new element</li> ← <li id="oldList">old element</li>)
-    parentNode.replaceChild(newList,oldList);
+    const parentElement = document.getElementById("list");
+    const elements = parentElement.getElementsByTagName("li");
+    const removeIndex = elements.length - 1;
+    if(removeIndex >= 0){
+        parentElement.removeChild(elements[removeIndex]);
+    }else{
+        console.log("no elements");
+    }
   },
   false
 );
