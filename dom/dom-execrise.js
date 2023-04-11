df01c7d3297bc2757734fcc61364f01e136eb1fb
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 14:29:57 +0900

    coding

--- dom/dom-execrise.js
+++ dom/dom-execrise.js
@@ -1,1 +1,21 @@
-'use strict'
+"use strict";
+
+const e = document.getElementById("button");
+e.addEventListener(
+  "click",
+  () => {
+    // nodeの生成(<li></li>)
+    const li = document.createElement("li");
+    // 入力された内容のnodeの生成
+    const text = document.getElementById("textbox");
+    const textNode = document.createTextNode(text.value);
+    // <li> 入力された内容 </li>
+    li.appendChild(textNode);
+
+    // <ul id="list"></ul>を持ってくる
+    const listElement = document.getElementById("list");
+    // <ul id="list"><li> 入力された内容 </li></ul>
+    listElement.appendChild(li);
+  },
+  false
+);
