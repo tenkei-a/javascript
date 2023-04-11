089054f24cb59f582887a2c2726986e6d6cae913
tenkei-a <k1.abe0423@gmail.com>
2023-04-11 11:36:18 +0900

    coding getElementsByTagName

--- dom/getElement.js
+++ dom/getElement.js
@@ -1,10 +1,30 @@
 "use strict";
 
 // getElementById
+// const e = document.getElementById("button");
+// e.addEventListener(
+//   "click",
+//   () => {
+//     // console.log('Clicked!');
+//     const result = document.getElementById("result");
+//     // result.innerText:id=resultの中身だけをとってくる
+//     console.log(result.innerText);
+//   },
+//   false
+// );
+
+// getElementsByTagName
 const e = document.getElementById("button");
-e.addEventListener("click", () => {
-    // console.log('Clicked!');
-    const result = document.getElementById('result');
-    // result.innerText:id=resultの中身だけをとってくる
-    console.log(result.innerText);
-}, false);
+e.addEventListener(
+    // クリック時に処理を行う
+  "click",
+  () => {
+    // クリック時の処理
+    // elementsには複数の要素が入ってくる。配列と似た要素が入ってくる。
+    const elements = document.getElementsByTagName("div");
+    for (let i = 0; i < elements.length; i++) {
+      console.log(elements[i].innerText);
+    }
+  },
+  false
+);
